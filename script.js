6a09b26ca0848cf9bdfe34d701fe6cde9b8ac336
Martim Norte <martimn1998@gmail.com>
2022-03-25 21:50:25 +0000

    Add buttons and result

--- script.js
+++ script.js
@@ -49,22 +49,22 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
 
-    // let score = 0;
+    let score = 0;
 
     // for (let i = 0; i < 5; i++) {
-    //     let playerSelection = prompt("Rock, paper or scissors?");
-    //     let computerSelection = computerPlay();
-    //     let result = playRound(playerSelection, computerSelection);
+    let playerSelection = prompt("Rock, paper or scissors?");
+    let computerSelection = computerPlay();
+    let result = playRound(playerSelection, computerSelection);
 
-    //     if (result === 'Win') {
-    //         score++;
-    //         console.log(`You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}!`);
-    //     }
-    //     else if (result === 'Lose') {
-    //         score--;
-    //         console.log(`You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}!`);
-    //     }
-    //     else console.log("It's a Tie!");
+    if (result === 'Win') {
+        score++;
+        console.log(`You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}!`);
+    }
+    else if (result === 'Lose') {
+        score--;
+        console.log(`You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}!`);
+    }
+    else console.log("It's a Tie!");
     // }
 
     // if (score < 0) {
@@ -76,7 +76,70 @@ function game() {
     // else console.log("You tied the match");
 }
 
-game();
+const selectors = document.querySelectorAll('.selector');
+const options = ['rock', 'paper', 'scissors'];
+const scoreBoard = document.querySelector('.score');
+const winnerDiv = document.querySelector('.winner');
+
+let result;
+let playerScore = 0;
+let computerScore = 0;
+
+function displayWinner(winner) {
+    if (winner === 'Player') {
+        winnerDiv.textContent = 'You win!'
+    }
+    else if (winner === 'Computer') {
+        winnerDiv.textContent = 'You Lose'
+    }
+}
+
+
+
+function endGame() {
+
+    selectors.forEach(element => {
+
+        element.removeEventListener('click', play);
+
+    });
+}
+
+function updateScore(result) {
+    if (result === 'Win') {
+        playerScore++;
+        if (playerScore === 5) {
+            displayWinner('Player');
+            endGame();
+        }
+    }
+    else if (result === 'Lose') {
+        computerScore++;
+        if (computerScore === 5) {
+            displayWinner('Computer');
+            endGame();
+        }
+    }
+
+    scoreBoard.textContent = `${playerScore} - ${computerScore}`;
+}
+
+function play(element) {
+    // checks which option was selected
+    outer: for (let i = 0; i < options.length; i++) {
+        if (element.currentTarget.contains(options[i])) {
+            result = playRound(options[i], computerPlay());
+            updateScore(result);
+            break outer;
+        }
+    }
+}
+
+selectors.forEach(element => {
+
+    element.addEventListener('click', play);
+
+});
 
 
 // console.log(computerPlay());
